c510f88ba381c09ebb9eb3bd6f601d35bffa8457
AMAN40011 <amanpal142005@gmail.com>
2026-08-21 00:17:19 +0530

    Build responsive landing page sections

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="max-w-7xl mx-auto px-8 py-20">
+      <h2 className="text-3xl md:text-4xl font-bold text-center">About us</h2>
+      <p className="mt-4 text-gray-600 text-center">
+        We create modern digital solutions that help businesses grow.
+      </p>
 
-export default About
+      <div className="flex flex-col md:flex-row items-center gap-10 mt-12">
+        <div className="w-full md:w-1/2  text-center md:text-left">
+          <h3 className="text-2xl font-semibold">
+            We Build Digital Experiences
+          </h3>
+          <p className="mt-4 text-gray-600">
+            Our team creates modern and reliable digital solutions for
+            businesses.
+          </p>
+          <button className="mt-6 bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition-colors duration-300 cursor-pointer">
+  Learn More
+</button>
+        </div>
+        <div className="w-full md:w-1/2">
+          <img
+            src="/about1.jpg"
+            alt="About"
+            className=" w-full h-64 md:h-80 object-cover rounded-xl shadow-lg hover:scale-105 transition-transform duration-300"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- Frontend/src/pages/Services.jsx
+++ Frontend/src/pages/Services.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { FaLaptopCode } from "react-icons/fa";
+import { FaMobileAlt } from "react-icons/fa";
+import { FaPaintBrush } from "react-icons/fa";
+import { FaCloud } from "react-icons/fa";
 
 const Services = () => {
   return (
@@ -9,10 +12,10 @@ const Services = () => {
       </h2>
 
       <div className="grid grid-cols-1 md:grid-cols-3  mt-12 gap-6">
-        <div className="border p-6 rounded-xl shadow-md hover:shadow-xl hover:-translate-y-1 transition-all duration-300">
+        <div className=" group border p-6 rounded-xl shadow-md hover:shadow-xl hover:-translate-y-1 transition-all duration-300">
           <div className="flex items-center gap-3">
-            <div className="bg-blue-100 p-2 rounded-lg">
-              <FaLaptopCode className="text-3xl text-blue-600" />
+            <div className="bg-blue-100 p-2 rounded-lg group-hover:bg-blue-600">
+              <FaLaptopCode className="text-3xl text-blue-600 group-hover:text-white" />
             </div>
 
             <h3 className="text-xl font-semibold">Web Development</h3>
@@ -21,15 +24,45 @@ const Services = () => {
             We build fast, responsive, and modern web applications.
           </p>
         </div>
-        <div className="border p-6 rounded-xl shadow-md hover:shadow-xl hover:-translate-y-1 transition-all duration-300">
-          Card 2
-        </div>
-        <div className="border p-6 rounded-xl shadow-md hover:shadow-xl hover:-translate-y-1 transition-all duration-300">
-          Card 3
-        </div>
-        <div className="border p-6 rounded-xl shadow-md hover:shadow-xl hover:-translate-y-1 transition-all duration-300">
-          Card 4
-        </div>
+       <div className="group border p-6 rounded-xl shadow-md hover:shadow-xl hover:-translate-y-1 transition-all duration-300">
+  <div className="flex items-center gap-3">
+    <div className="bg-blue-100 p-2 rounded-lg group-hover:bg-blue-600">
+      <FaMobileAlt className="text-3xl text-blue-600 group-hover:text-white" />
+    </div>
+
+    <h3 className="text-xl font-semibold">Mobile Development</h3>
+  </div>
+
+  <p className="mt-3 text-gray-600">
+    We create modern and responsive mobile applications.
+  </p>
+</div>
+        <div className="group border p-6 rounded-xl shadow-md hover:shadow-xl hover:-translate-y-1 transition-all duration-300">
+  <div className="flex items-center gap-3">
+    <div className="bg-blue-100 p-2 rounded-lg group-hover:bg-blue-600">
+      <FaPaintBrush className="text-3xl text-blue-600 group-hover:text-white" />
+    </div>
+
+    <h3 className="text-xl font-semibold">UI/UX Design</h3>
+  </div>
+
+  <p className="mt-3 text-gray-600">
+    We design clean and engaging digital experiences.
+  </p>
+</div>
+         <div className="group border p-6 rounded-xl shadow-md hover:shadow-xl hover:-translate-y-1 transition-all duration-300">
+  <div className="flex items-center gap-3">
+    <div className="bg-blue-100 p-2 rounded-lg group-hover:bg-blue-600">
+      <FaCloud className="text-3xl text-blue-600 group-hover:text-white" />
+    </div>
+
+    <h3 className="text-xl font-semibold">Cloud Solutions</h3>
+  </div>
+
+  <p className="mt-3 text-gray-600">
+    We provide scalable and reliable cloud solutions.
+  </p>
+</div>
       </div>
     </section>
   );
