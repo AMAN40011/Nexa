0588d51f7f40e2f7c5bbb04a93c969a0d5eb05c6
AMAN40011 <amanpal142005@gmail.com>
2026-08-30 14:12:07 +0530

    Change the cookies config

--- Backend/jwt/token.js
+++ Backend/jwt/token.js
@@ -7,11 +7,11 @@ export const generateJwtToken =  (id, res) => {
     });
 
     res.cookie("jwt", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
     
   } catch (error) {
     return res
